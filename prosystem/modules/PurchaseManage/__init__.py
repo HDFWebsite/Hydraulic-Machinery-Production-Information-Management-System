a4a2b8bf3c6c9e721feca410676a49ce7b3ab09d
HDFWebsite <2978326915@qq.com>
2019-04-18 19:52:30 +0800

    蓝图

--- prosystem/modules/PurchaseManage/__init__.py
+++ prosystem/modules/PurchaseManage/__init__.py
@@ -5,5 +5,4 @@ from flask import Blueprint
 purchases_blue = Blueprint('purchases_blue',__name__)
 
 # 把使用蓝图对象的文件，导入到创建蓝图对象的下面
-from . import purchases1
-from . import purchases2
+from . import purchases
